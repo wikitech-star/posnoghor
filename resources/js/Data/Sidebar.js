3987aa365aa78aa8de510240ce6e83d06e80f136
Sahab Uddin Ahmad <sahabuab@gmail..com>
2025-10-09 21:31:37 +0600

    Update sidebar menu

--- resources/js/Data/Sidebar.js
+++ resources/js/Data/Sidebar.js
@@ -10,6 +10,8 @@ const ADMIN_SIDEBAR = [
         icon: Home,
         link: route("ux.dashboard"),
         active: ["ux.dashboard"],
+        'role': ['admin', 'editor', 'support'],
+        'pro': false
     },
     {
         title: "স্কুল",
@@ -20,24 +22,33 @@ const ADMIN_SIDEBAR = [
         icon: University,
         link: route("ux.group.class"),
         active: ["ux.group.class"],
+        'role': ['admin', 'editor', 'support'],
+        'pro': false,
+        'pro_link': route('home')
     },
     {
         name: "সকল বিষয়",
         icon: Book,
         link: route("ux.subjects"),
         active: ["ux.subjects"],
+        'role': ['admin', 'editor', 'support'],
+        'pro': false
     },
     {
         name: "সকল অধ্যায়",
         icon: BookOpenText,
         link: route("ux.lassion"),
         active: ["ux.lassion"],
+        'role': ['admin', 'editor', 'support'],
+        'pro': false
     },
     {
         name: "প্রশ্নের ধরন",
         icon: FileQuestionMark,
         link: route("ux.question.type"),
         active: ["ux.question.type"],
+        'role': ['admin', 'editor', 'support'],
+        'pro': false
     },
     {
         title: "ব্যাংক",
@@ -48,12 +59,16 @@ const ADMIN_SIDEBAR = [
         icon: BadgeQuestionMark,
         link: route("ux.question.type"),
         active: ["ux.question.type"],
+        'role': ['admin', 'editor', 'support'],
+        'pro': false
     },
     {
         name: "নতুন প্রশ্ন",
         icon: BadgePlus,
         link: route("ux.question.add"),
         active: ["ux.question.add"],
+        'role': ['admin', 'editor', 'support'],
+        'pro': false
     },
     {
         title: "সেটিংস",
@@ -64,18 +79,24 @@ const ADMIN_SIDEBAR = [
         icon: Settings,
         link: route("ux.site.setting"),
         active: ["ux.site.setting"],
+        'role': ['admin'],
+        'pro': false
     },
     {
         name: "ইমেইল সেটিংস",
         icon: MailWarning,
         link: route("ux.mail.setting"),
         active: ["ux.mail.setting"],
+        'role': ['admin'],
+        'pro': false
     },
     {
         name: "গুগল সেটিংস",
         icon: Key,
         link: route("ux.goolge.auth.setting"),
         active: ["ux.goolge.auth.setting"],
+        'role': ['admin'],
+        'pro': false
     },
 ];
 
